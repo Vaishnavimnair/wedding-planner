9433df3d78718662f6b4a3316605d9aff61c0561
Vaishnavimnair <mvaishnavinair2001@gmail.com>
2025-05-27 12:12:33 +0530

    first commit

---
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+export default function Navigation() {
+    return (
+        <div>
+            <header>
+                <div>
+                <h1>EVENTIQUE</h1>
+                <h3>dine and design</h3>
+                </div>
+                
+                <div>
+                    <a href="">Home</a>
+                    <a href="">About us</a>
+                </div>
+            </header>
+        </div>
+    );
+}
